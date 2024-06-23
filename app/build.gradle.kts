ff160b144824a86436808fba2a4ce7dd494cb7a1
emad <emadabdalrahman11@gmail.com>
2024-06-23 12:26:34 +0300

    Add hardware lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(files("libs/kioskhardware-0.0.17.aar"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
